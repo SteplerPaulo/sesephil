b12106eb344e58b16d53abdcd26c3920f4113f46
Paulo <paulobiscocho>
2016-08-05 19:12:20 +0800

    Update database

--- config/database.php
+++ config/database.php
@@ -1,13 +1,26 @@
 <?php
 class DATABASE_CONFIG {
-
+	var $remote = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'mysql.hostinger.ph',
+		'login' => 'u519358519_ssphl',
+		'password' => 'Ch0wK1ing',
+		'database' => 'u519358519_ssphl',
+	);
+	
 	var $default = array(
 		'driver' => 'mysql',
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'sesephil',
+		'database' => 'sesephil_updated',
 	);
+	function __construct() {
+		if($_SERVER['HTTP_HOST']!='localhost'){
+			$this->default =  $this->remote;
+		}
+	}
 }
 ?>
