0cd1a6d7595c0f369005e96b1c2240823b75778f
Paulo <paulobiscocho>
2016-08-05 18:19:03 +0800

    Admin User JS - Allowing & Removing User As Admin

--- webroot/js/controllers/admin_users.js
+++ webroot/js/controllers/admin_users.js
@@ -10,4 +10,21 @@ App.controller('AdminUsersController',function($scope,$rootScope,$http,$filter){
 		});
 	}
 	
+	$scope.addAsAdmin = function (data){
+		$scope.UserId = data.User.id;
+		$scope.ArosId = 2;//AROS ID FOR ADMIN GROUP
+		
+		
+		$scope.ModalMessage = 'Assign '+data.User.full_name +' as admin?';
+		$('#Modal').modal('show');
+		
+	}
+	
+	$scope.removeAsAdmin = function (data){
+		$scope.UserId = data.User.id;
+		$scope.ArosId = 3;//AROS ID FOR USER GROUP
+		
+		$scope.ModalMessage = 'Remove '+data.User.full_name +' as admin?';
+		$('#Modal').modal('show');
+	}
 });
