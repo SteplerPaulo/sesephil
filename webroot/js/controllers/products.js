37145a6624a815c79e19d5c2e7b5efa2ae2b362f
Paulo <paulobiscocho>
2016-08-05 20:42:01 +0800

    Product Js Base URL

--- webroot/js/controllers/products.js
+++ webroot/js/controllers/products.js
@@ -1,14 +1,15 @@
+var BASE_URL = window.location.href;
 App.controller('ProductsController',function($scope,$rootScope,$http,$filter){
 	
 	$scope.initializeController = function(){
 		$scope.currentPage = 1; 
 		$scope.pageSize =12;
 		
-		$http.get("/sesephil/categories/main_children").success(function(response) {
+		$http.get(BASE_URL+"categories/main_children").success(function(response) {
 			$scope.categories = response;
 		});
 			
-		$http.get("/sesephil/products/all").success(function(response) {
+		$http.get(BASE_URL+"products/all").success(function(response) {
 			$scope.products = response;
 			
 			if(window.location.pathname.split('/')[3]){
