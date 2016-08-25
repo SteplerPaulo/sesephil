c4e914c144fa87c407291b0179effc35f7f71db9
Paulo <paulobiscocho>
2016-08-25 12:02:15 +0800

    DB CONFIG

--- config/database.php
+++ config/database.php
@@ -1,26 +1,14 @@
 <?php
 class DATABASE_CONFIG {
-	var $remote = array(
-		'driver' => 'mysql',
-		'persistent' => false,
-		'host' => 'mysql.hostinger.ph',
-		'login' => 'u519358519_ssphl',
-		'password' => 'Ch0wK1ing',
-		'database' => 'u519358519_ssphl',
-	);
-	
+
 	var $default = array(
 		'driver' => 'mysql',
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'sesephil_updated',
+		'database' => 'sesephil',
 	);
-	function __construct() {
-		if($_SERVER['HTTP_HOST']!='localhost'){
-			$this->default =  $this->remote;
-		}
-	}
+	
 }
 ?>
