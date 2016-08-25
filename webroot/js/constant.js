8e8403e0b56efa527b93b2f7c0e46159721108d6
Paulo <paulobiscocho>
2016-08-25 12:00:58 +0800

    constant adjusted

--- webroot/js/constant.js
+++ webroot/js/constant.js
@@ -1,4 +1,4 @@
-if (document.location.hostname == "localhost"){
+if (document.location.hostname == "localhost" || document.location.hostname == "192.168.1.52"){
 	var BASE_URL = '/sesephil/';
 }else{
 	var	BASE_URL = 'http://sesephil.hol.es/';
